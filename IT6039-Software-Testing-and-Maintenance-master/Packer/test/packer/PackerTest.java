fe6bfa6450ecfa9bd16007774d9ed6ec5c6d830c
91031442 <91031442@LRNWNWRK-051.learn.cpp.ac.nz>
2020-01-15 10:34:22 +1300

    Made Packer test file

---
+++ IT6039-Software-Testing-and-Maintenance-master/Packer/test/packer/PackerTest.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packer;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 91031442
+ */
+public class PackerTest {
+    
+    public PackerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of packProducts method, of class Packer.
+     */
+    @Test
+    public void testPackProducts() {
+        System.out.println("packProducts");
+        Address customerAddress1 = new Address("67 Torch Rd", "Tree line", "Mt High", "799", new Coordinates(1102, 87));
+        Customer c = new Customer("Andy Bravo", customerAddress1);
+        Address depotAddress = new Address("23 Good Luck St", "Blue View", "Sandy Shores", "H337", new Coordinates(138, 995));
+        Depot d = new Depot("Main Depot", depotAddress);
+        Manifest m = new Manifest();
+        m.addProduct(new Product("Nails", 1, false, false), 4);
+        List<Box> expResult = Packer.packProducts(c, d, m);
+        Manifest n = new Manifest();
+        m.addProduct(new Product("Nails", 1, false, false), 4);
+        List<Box> result = Packer.packProducts(c, d, n);
+        assertEquals(expResult, result);
+    }
+    
+}
