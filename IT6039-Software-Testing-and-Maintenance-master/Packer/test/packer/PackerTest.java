96a64bb346c9d25eb2a44df405135ac2495a3c79
91031442 <91031442@LRNWNWRK-051.learn.cpp.ac.nz>
2020-01-16 09:01:36 +1300

    added to Packer test file

--- IT6039-Software-Testing-and-Maintenance-master/Packer/test/packer/PackerTest.java
+++ IT6039-Software-Testing-and-Maintenance-master/Packer/test/packer/PackerTest.java
@@ -46,15 +46,23 @@ public class PackerTest {
         System.out.println("packProducts");
         Address customerAddress1 = new Address("67 Torch Rd", "Tree line", "Mt High", "799", new Coordinates(1102, 87));
         Customer c = new Customer("Andy Bravo", customerAddress1);
+        
         Address depotAddress = new Address("23 Good Luck St", "Blue View", "Sandy Shores", "H337", new Coordinates(138, 995));
         Depot d = new Depot("Main Depot", depotAddress);
+        
         Manifest m = new Manifest();
-        m.addProduct(new Product("Nails", 1, false, false), 4);
-        List<Box> expResult = Packer.packProducts(c, d, m);
-        Manifest n = new Manifest();
-        m.addProduct(new Product("Nails", 1, false, false), 4);
-        List<Box> result = Packer.packProducts(c, d, n);
-        assertEquals(expResult, result);
+        m.addProduct(new Product("Nails", 1, false, false), 12);
+        m.addProduct(new Product("Saw", 5, false, false), 1);
+        List<Box> result1 = Packer.packProducts(c, d, m);
+        assertEquals(true, result1.equals(result1));
+        
+        Manifest m1 = new Manifest();
+        m1.addProduct(new Product("Nails", 1, false, false), 4);
+        m1.addProduct(new Product("Ladder", 15, false, false), 1);
+        m1.addProduct(new Product("Light Bulbs", 1, false, true), 1);
+        List<Box> result2 = Packer.packProducts(c, d, m1);
+        assertEquals(true, result2.equals(result2));
+        
     }
     
 }
